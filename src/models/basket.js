38bde9f45fb8f710a13ac7f6c112ec90d38fc019
Christine Frank and Jennifer Batara <jennifer@>
2019-09-09 14:05:42 -0700

    Change seeds to be an obj, Updated addSeeds, sellPlant
    -Change seeds to be an object to easily locate different seed types
    -Updated addSeeds to account for quantity bought
    -Update sellPlant to return goldEarned

--- src/models/basket.js
+++ src/models/basket.js
@@ -1,46 +1,54 @@
 export class Basket {
-  constructor(){
+  constructor() {
     this.maturePlants = [];
     this.gold = 30;
-    this.seeds = [
-      ["Tomato", 4],
-      ["Carrot", 4],
-      ["Sunflower", 4],
-      ["Pumpkin", 2]
-    ];
+    this.seeds = {
+      tomato: 4,
+      carrot: 4,
+      sunflower: 4,
+      pumpkin: 2
+    };
     this.tools = [];
   }
 
-  addTool(tool, price){
-    if(price <= this.gold){
+  addTool(tool, price) {
+    if (price <= this.gold) {
       this.tools.push(tool);
       exchangeGold(-price);
     }
   }
 
-  addPlants(plants){
+  addPlants(plants) {
     this.maturePlants = this.maturePlants.concat(plants);
   }
 
-  addSeeds(seeds, totalCost){
-    if(totalCost <= this.gold){
-      this.seeds = this.seeds.concat(seeds);
-      exchangeGold(-totalCost);
+  addSeeds(seedName, price, qty) {
+    if(price*qty <= this.gold){
+      if (this.seeds[seedName]) {
+        this.seeds[seedName]+= qty;
+        exchangeGold(-(price * qty));
+      }else{
+        this.seeds[seedName]= qty;
+        exchangeGold(-(price * qty));
+      }
     }
   }
 
-  sellPlant(plant, price){
+  sellPlant(plant, price) {
     this.maturePlants.sort();
     const plantFirstIndex = this.maturePlants.indexOf(plant);
     const plantLastIndex = this.maturePlants.lastIndexOf(plant);
     const indexDelta = plantLastIndex - plantFirstIndex;
-    if(plantIndex !== -1){
-      this.maturePlants.splice(plantFirstIndex, indexDelta+1);
-      exchangeGold(price*(indexDelta+1));
+    let goldEarned=0;
+    if (plantIndex !== -1) {
+      this.maturePlants.splice(plantFirstIndex, indexDelta + 1);
+      exchangeGold(price * (indexDelta + 1));
+      goldEarned = price * (indexDelta + 1);
     }
+    return goldEarned;
   }
 
-  exchangeGold(goldAmount){
+  exchangeGold(goldAmount) {
     this.gold += goldAmount;
   }
 }
