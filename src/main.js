da13aa3129a0e92af901beb1559cc7897ed2e6c0
Christine Frank and Jennifer Batara <christine@>
2019-09-10 11:55:06 -0700

    Add comments for needed actions/features in UI

--- src/main.js
+++ src/main.js
@@ -3,3 +3,25 @@ import $ from 'jquery';
 $(document).ready(function(){
   // $('body').html("<img src='https://media.giphy.com/media/xT5LMWxAjHP2kHrNN6/giphy.gif'>");
 });
+
+
+// updateGardenDisplay
+    // plant icon
+    // stats: water count, maturity level, life span to next stage
+
+    // separate actions: water all plant, pull all dead plants, pull all weeds with selected tool, plant all of single type seeds, harvest all plants of selected type
+
+
+// display Basket options/actions
+  // gold amount
+  // seeds and qty
+  // harvested plants -> actions: dry / sell
+  // purchased tools
+
+// display Store options/actions
+  // seeds and cost -> action: buy
+  // tools and cost -> action: buy
+
+// display day
+// display last action
+// button to trigger next day
